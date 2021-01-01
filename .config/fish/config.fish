10ab21e5c10a8e7d8dff25f72ffdc3a54ab99759
vhertz <v3hertz@gmail.com>
2021-01-01 20:49:15 +0900

    [config.fish] Remove fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,14 +14,6 @@ if test -e $LOCAL_CONF
     source $LOCAL_CONF
 end
 
-# Install fishser
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs \
-         -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 # common aliases
 if test (uname) = "Darwin"
     alias ls="ls -G"
