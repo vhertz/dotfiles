da12a63b828aa7c5b4e50bd8b013449b87bc9ecb
vhertz <v3hertz@gmail.com>
2024-06-04 20:06:56 +0900

    [.config/nvim] add mason-null-ls plugin

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,20 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-            },
-        })
-
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+          "williamboman/mason.nvim",
+          "nvimtools/none-ls.nvim",
+        },
+        opts = {
+            handlers = {}
+        },
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            require("null-ls").setup()
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
 }
