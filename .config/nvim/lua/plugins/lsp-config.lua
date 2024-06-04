ad842558980258e3da2e33fe3d043492c41ab778
vhertz <v3hertz@gmail.com>
2024-06-04 19:22:01 +0900

    [.config/nvim] no language server installed by default

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,9 +8,7 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "ruff" },
-            })
+            require("mason-lspconfig").setup()
         end,
     },
     {
@@ -19,11 +17,12 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
             })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
